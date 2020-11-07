68685bfc95b17afd6a80df431ce91d87e135867f
ZachMac <zachtball@gmail.com>
2020-11-07 01:44:42 -0700

    fixed app init auth

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import { AuthRedirect, Authenticated, Test, Navigation } from './components';
+import React, { useState } from 'react';
+import { AuthRedirect, Test, Navigation } from './components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import { Login } from './views';
 import './styles/main.scss';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [authenticated, setAuthenticated] = useState(
+    Boolean(localStorage.getItem('REDDIT_TOKEN'))
+  );
+
+  if (
+    !authenticated &&
+    !loading &&
+    window.location.href.split('?')[0] !==
+      `${window.location.origin}/auth-redirect`
+  ) {
+    return <Login />;
+  }
+
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path='/auth-redirect'>
-            <AuthRedirect />
+            <AuthRedirect
+              setAuthenticated={setAuthenticated}
+              setLoading={setLoading}
+            />
           </Route>
-          <Route exact path='/login'>
-            <Login color='primary' />
-          </Route>
-          <Authenticated>
-            <Navigation />
-            <Route exact path='/test'>
-              <Test />
-            </Route>
-          </Authenticated>
+          {authenticated && !loading && (
+            <>
+              <Navigation />
+              <Route exact path='/test'>
+                <Test />
+              </Route>
+            </>
+          )}
         </Switch>
       </div>
     </Router>
--- src/components/AuthRedirect/index.js
+++ src/components/AuthRedirect/index.js
@@ -1,24 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { getToken } from '../../api/authApi';
+import { getMe, getToken } from '../../api';
 
-export default () => {
+export default ({ setAuthenticated, setLoading }) => {
+  const [token, setToken] = useState(null);
   const history = useHistory();
   const codeParam = new URLSearchParams(window.location.href).get('code');
   useEffect(() => {
+    setLoading(true);
     if (codeParam) {
       getToken(codeParam)
         .then((res) => {
           localStorage.setItem('REDDIT_TOKEN', res.data);
-
-          history.push('/');
+          setToken(localStorage.getItem('REDDIT_TOKEN'));
         })
         .catch((err) => {
           console.log('error authenticating', err);
+          setLoading(false);
           history.push('/login');
         });
     }
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      setAuthenticated(true);
+      getMe().then(({ data }) => {
+        console.log(data);
+        setLoading(false);
+        history.push('/');
+      });
+    }
+  }, [token]);
 
   return <>THIS IS AUTH REDIRECT</>;
 };
