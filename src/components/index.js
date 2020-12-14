de2e851e57e2e0b9301d367e545209273f66f286
ZachMac <zachtball@gmail.com>
2020-12-13 17:02:29 -0700

    fix index

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-export { default as Authenticated } from './Authenticated';
 export { default as AuthRedirect } from './AuthRedirect';
 export { default as Navigation } from './Navigation';
 
